5a009d6b9498b6d66d07c7eb3865591c1e4119cf
aligh <aghasizadeh70@gmail.com>
2012-04-07 15:01:47 +0430

    form validat

--- javascript/js.js
+++ javascript/js.js
@@ -1,1 +1,37 @@
-// JavaScript Document
+window.onload = function(){
+	var ret = true;
+	var myform = document.forms.item(0);
+	myform.onsubmit = function(){
+		for (var i=0;i<myform.elements.length;i++){
+			if(myform.elements.item(i).className!='ok'){
+				ret = false;
+				break;
+			}
+		}
+		return ret;	
+	}
+	
+	for(var i=0;i<myform.elements.length;i++){
+		var inp = myform.elements.item(i);
+		if (inp.type!='text' && inp.type!='textarea'){
+			continue;
+		}
+		
+		inp.onfocus = function(){
+			if (this.value==this.defaultValue){
+				this.value='';
+			}
+		}
+		
+		inp.onblur = function(){
+			if (this.value!=''){
+				this.className='ok'
+			}else{
+				this.className='nok'
+			}
+			if (this.value==''){
+				this.value=this.defaultValue;
+			}
+		}
+	}
+}
